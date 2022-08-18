a4e864b6552ef9f9b6abaeb233d0f349edeaff8e
Muammer Akca <muammerakca10@gmail.com>
2022-08-18 16:14:59 +0300

    TextView and Button added to AddViewController

--- ToDo3/AddViewController.swift
+++ ToDo3/AddViewController.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class AddViewController: UIViewController {
     
+    @IBOutlet var addTextFieldText: UITextView!
+    
     var newTask = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addTextFieldText.layer.masksToBounds = true
+        addTextFieldText.layer.cornerRadius = 20.0
+        addTextFieldText.layer.borderWidth = 1
+        addTextFieldText.layer.borderColor = UIColor.blue.cgColor
+        addTextFieldText.textAlignment = NSTextAlignment.left
+        addTextFieldText.dataDetectorTypes = .all
+        addTextFieldText.isEditable = true
 
         
+    }
+    @IBAction func addButtonTapped(_ sender: Any) {
+        NotificationCenter.default.post(name: NSNotification.Name("Data_Saved"), object: nil)
+        
+        self.navigationController?.popViewController(animated: true)
     }
     
 }
